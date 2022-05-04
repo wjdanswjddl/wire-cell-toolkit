2d28dd3ff988fc05ece6636470423a376cef7ab6
HaiwangYu <yuhw.pku@gmail.com>
2022-05-03 23:55:26 -0400

    interface to merge two slices

--- img/inc/WireCellImg/ImgData.h
+++ img/inc/WireCellImg/ImgData.h
@@ -49,6 +49,7 @@ namespace WireCell {
 
                 void sum(const IChannel::pointer& ch, value_t val) { m_activity[ch] += val; }
                 void assign(const IChannel::pointer& ch, value_t val) { m_activity[ch] = val; }
+                void merge(const ISlice::pointer& in) {m_activity.insert(in->activity().begin(),in->activity().end());}
             };
 
             // simple collection
