f56e1859d3cf56e0f7e860923a6f56dc00a855a1
Brett Viren <brett.viren@gmail.com>
2022-05-31 12:00:42 -0400

    Break Nin x Nout complexity of templates

--- tbb/inc/WireCellTbb/HydraCat.h
+++ tbb/inc/WireCellTbb/HydraCat.h
@@ -9,12 +9,23 @@
 #ifndef WIRECELLTBB_HYDRACAT
 #define WIRECELLTBB_HYDRACAT
 
-#include "WireCellIface/IHydraNode.h"
 #include "WireCellTbb/NodeWrapper.h"
+#include "WireCellIface/IHydraNode.h"
+#include "WireCellUtil/Exceptions.h"
 
 namespace WireCellTbb {
 
-    template<typename T, unsigned N, typename... REST>
+    /// The "any" interface to IHydraNode acccepts this type for both
+    /// input and output.
+    using any_queue_vector = WireCell::IHydraNodeBase::any_queue_vector;
+
+    /// The TBB hydra implementation consists of an input side and an
+    /// output side:
+
+    /// Hydra input
+
+
+    template<typename T, std::size_t N, typename... REST>
     struct generate_indexer_node
     {
         typedef typename generate_indexer_node<T, N-1, T, REST...>::type type;
@@ -24,7 +35,59 @@ namespace WireCellTbb {
         typedef tbb::flow::indexer_node<T, REST...> type;
     };
 
-    template<typename InT, typename T, unsigned N, typename... REST>
+    template <typename IndexerNode, std::size_t... Is>
+    receiver_port_vector indexer_ports(IndexerNode& dxn, std::index_sequence<Is...>)
+    {
+        return {dynamic_cast<receiver_type*>(&tbb::flow::input_port<Is>(dxn))...};
+    }
+
+    template<std::size_t Nin>
+    using indexer_node = typename generate_indexer_node<msg_t, Nin>::type;
+
+    template<std::size_t Nin>
+    using indexer_msg_t = typename indexer_node<Nin>::output_type;
+
+    // Erase the Nin.
+    using tagged_msg_t = std::pair<size_t, msg_t>;
+
+    template<std::size_t Nin>
+    struct HydraInputBody {
+
+        tagged_msg_t operator()(const indexer_msg_t<Nin>& in) const
+        {
+            const std::size_t index = in.tag();
+            const msg_t& msg = in.template cast_to<msg_t>();
+            return std::make_pair(index, msg);
+        }
+
+    };
+
+    // Construct the hydra input of port-cardinality Nin.  This is a
+    // subgraph of an indexer node followed by a function node to
+    // erase the Nin template argument to break Nin X Nout complexity.
+    // The subgraph is added to nodes in order of input to output.
+    template <std::size_t Nin>
+    tbb::flow::sender<tagged_msg_t>* // the output of the input
+    build_hydra_input(tbb::flow::graph& graph,
+                      receiver_port_vector& receivers,
+                      std::vector<tbb::flow::graph_node*>& nodes)
+    {
+        auto* dxn = new indexer_node<Nin>(graph);
+        nodes.push_back(dxn);
+        receivers = indexer_ports(*dxn, std::make_index_sequence<Nin>{});
+
+        auto* fn = new tbb::flow::function_node<indexer_msg_t<Nin>, tagged_msg_t>(graph, 1, HydraInputBody<Nin>{});
+        nodes.push_back(fn);
+        make_edge(*dxn, *fn);
+
+        return fn;
+    }
+
+
+    /// Hydra output
+
+
+    template<typename InT, typename T, std::size_t N, typename... REST>
     struct generate_outdexer_node
     {
         typedef typename generate_outdexer_node<InT, T, N-1, T, REST...>::type type;
@@ -35,110 +98,127 @@ namespace WireCellTbb {
     };
 
 
-    template <typename IndexerNode, std::size_t... Is>
-    receiver_port_vector indexer_ports(IndexerNode& dxn, std::index_sequence<Is...>)
-    {
-        return {dynamic_cast<receiver_type*>(&tbb::flow::input_port<Is>(dxn))...};
-    }
-
     template<typename OutdexerNode, std::size_t... Is>
     sender_port_vector outdexer_ports(OutdexerNode& mfn, std::index_sequence<Is...>)
     {
         return {dynamic_cast<sender_type*>(&tbb::flow::output_port<Is>(mfn))...};
     }
 
+    template<std::size_t Nout>
+    using mfunc_node_type = typename generate_outdexer_node<tagged_msg_t, msg_t, Nout>::type;
 
-    template<int Nin>
-    using indexer_node = typename generate_indexer_node<msg_t, Nin>::type;
+    template<std::size_t Nout>
+    using mfunc_ports_type = typename mfunc_node_type<Nout>::output_ports_type;
 
-    template<int Nin>
-    using indexer_msg_t = typename indexer_node<Nin>::output_type;
+    template<std::size_t Nout, std::size_t N=0>
+    void try_transfer_n(mfunc_ports_type<Nout>& out, 
+                        const any_queue_vector& oqv,
+                        std::vector<size_t>& counts)
+    {
+        for (const auto& oq : oqv) {
+            for (const auto& o : oq) {
+                bool accepted = std::get<N>(out).try_put(msg_t(counts[N]++, o));
+                if (!accepted) {
+                    std::cerr << "TbbFlow: hydra node input return false when try put on " << N << "\n";
+                }
+            }
+        }
+        if constexpr (N + 1 < Nout) {
+            try_transfer_n<Nout, N + 1>(out, oqv, counts);
+        }
+    }
 
-    template<int Nin, int Nout>
-    using mfunc_node_type = typename generate_outdexer_node<indexer_msg_t<Nin>, msg_t, Nout>::type;
-
-    template<int Nin, int Nout>
-    using mfunc_ports_type = typename mfunc_node_type<Nin,Nout>::output_ports_type;
-
-
-    template <std::size_t Nin, std::size_t Nout>
-    class HydraBody {
+    template <std::size_t Nout>
+    class HydraOutputBody {
         WireCell::IHydraNodeBase::pointer m_wcnode;
-
+        mutable std::vector<seqno_t> m_seqnos;
+        const size_t nin;
       public:
-        HydraBody(WireCell::INode::pointer wcnode)
+        HydraOutputBody(WireCell::INode::pointer wcnode)
+            : m_seqnos(Nout, 0)
+            , nin(wcnode->input_types().size())
         {
             m_wcnode = std::dynamic_pointer_cast<WireCell::IHydraNodeBase>(wcnode);
         }
-        void operator()(const indexer_msg_t<Nin>& in, mfunc_ports_type<Nin,Nout>& out)
+        void operator()(const tagged_msg_t& in, mfunc_ports_type<Nout>& out)
         {
+            any_queue_vector iqv(nin), oqv(Nout);
+
+            size_t index = in.first;
+            iqv[index].push_back(in.second);
+
+            bool ok = (*m_wcnode)(iqv, oqv);
+            if (!ok) {
+                std::cerr << "TbbFlow: hydra body return false ignored\n";
+            }
+            try_transfer_n<Nout>(out, oqv, m_seqnos);
         }
     };
 
-    using rxtx_port_vectors = std::pair<receiver_port_vector, sender_port_vector>;
-
-    template <std::size_t Nin, std::size_t Nout>
-    rxtx_port_vectors build_hydra(tbb::flow::graph& graph,
-                                  WireCell::INode::pointer wcnode,
-                                  std::vector<tbb::flow::graph_node*>& nodes)
+    template <std::size_t Nout>
+    tbb::flow::receiver<tagged_msg_t>* // the input to the output
+    build_hydra_output(tbb::flow::graph& graph,
+                       WireCell::INode::pointer wcnode,
+                       sender_port_vector& senders,
+                       std::vector<tbb::flow::graph_node*>& nodes)
     {
-
-        auto* dxn = new indexer_node<Nin>(graph);
-        nodes.push_back(dxn);
-        receiver_port_vector recv = indexer_ports(*dxn, std::make_index_sequence<Nin>{});
-
-
-        auto mfn = new mfunc_node_type<Nin, Nout>(graph, 1, HydraBody<Nin,Nout>(wcnode));
-        tbb::flow::make_edge(*dxn, *mfn);
+        auto mfn = new mfunc_node_type<Nout>(graph, 1, HydraOutputBody<Nout>(wcnode));
+        nodes.push_back(mfn);
 
         auto spv = outdexer_ports(*mfn, std::make_index_sequence<Nout>{});
 
-        sender_port_vector send;
-        std::vector<seq_node*> seq_nodes;
         for (size_t ind=0; ind<Nout; ++ind) {
             auto qn = new seq_node(graph, [](const msg_t& m) {return m.first;});
-            seq_nodes.push_back(qn);
+            nodes.push_back(qn);
             tbb::flow::make_edge(*spv[ind], *qn);
-            send.push_back(dynamic_cast<sender_type*>(qn));
+            senders.push_back(dynamic_cast<sender_type*>(qn));
         }
-        return std::make_pair(recv, send);
+        return mfn;
     }
+
 
     // Wrap TBB around WCT hydra node
     class HydraWrapper : public NodeWrapper {
         std::vector<tbb::flow::graph_node*> m_nodes;
-        rxtx_port_vectors m_rxtx;
+        receiver_port_vector m_rx;
+        sender_port_vector m_tx;
       public:
         HydraWrapper(tbb::flow::graph& graph, WireCell::INode::pointer wcnode)
         {
             const size_t nin = wcnode->input_types().size();
             const size_t nout = wcnode->output_types().size();
 
-            Assert(nin > 0 && nin <= 3);
-            Assert(nout > 0 && nout <= 3);
-            if (1 == nin) {
-                if (1 == nout) m_rxtx = build_hydra<1,1>(graph, wcnode, m_nodes);
-                if (2 == nout) m_rxtx = build_hydra<1,2>(graph, wcnode, m_nodes);
-                if (3 == nout) m_rxtx = build_hydra<1,3>(graph, wcnode, m_nodes);
+            tbb::flow::sender<tagged_msg_t>* fe;
+            if(nin < 1 || nin > 3) {
+                THROW(WireCell::ValueError() << WireCell::errmsg{"unsupported size for hydra input"});
             }
-            if (2 == nin) {
-                if (1 == nout) m_rxtx = build_hydra<2,1>(graph, wcnode, m_nodes);
-                if (2 == nout) m_rxtx = build_hydra<2,2>(graph, wcnode, m_nodes);
-                if (3 == nout) m_rxtx = build_hydra<2,3>(graph, wcnode, m_nodes);
+            if (1 == nin) fe = build_hydra_input<1>(graph, m_rx, m_nodes);
+            if (2 == nin) fe = build_hydra_input<2>(graph, m_rx, m_nodes);
+            if (3 == nin) fe = build_hydra_input<3>(graph, m_rx, m_nodes);
+            // ... more if needed
+
+
+            tbb::flow::receiver<tagged_msg_t>* be;
+            if(nout < 1 || nout > 3) {
+                THROW(WireCell::ValueError() << WireCell::errmsg{"unsupported size for hydra output"});
             }
-            if (3 == nin) {
-                if (1 == nout) m_rxtx = build_hydra<3,1>(graph, wcnode, m_nodes);
-                if (2 == nout) m_rxtx = build_hydra<3,2>(graph, wcnode, m_nodes);
-                if (3 == nout) m_rxtx = build_hydra<3,3>(graph, wcnode, m_nodes);
-            }
+            if (1 == nout) be = build_hydra_output<1>(graph, wcnode, m_tx, m_nodes);
+            if (2 == nout) be = build_hydra_output<2>(graph, wcnode, m_tx, m_nodes);
+            if (3 == nout) be = build_hydra_output<3>(graph, wcnode, m_tx, m_nodes);
+            // ... more if needed
+
+            // Join input front-end half to output back-end half
+            tbb::flow::make_edge(*fe, *be);
         }
+
         virtual sender_port_vector sender_ports()
         {
-            return m_rxtx.second;
+            return m_tx;
         }
+
         virtual receiver_port_vector receiver_ports()
         {
-            return m_rxtx.first;
+            return m_rx;
         }          
     };
 }
