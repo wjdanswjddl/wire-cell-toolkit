dc50aa8b5e679c0a3a1b04d5bd1ab2ffe2c3be16
HaiwangYu <yuhw.pku@gmail.com>
2023-05-19 10:37:41 -0400

    add examples

--- img/src/InSliceDeghosting.cxx
+++ img/src/InSliceDeghosting.cxx
@@ -65,7 +65,8 @@ namespace {
     }
 
     template <class Map, class Key, class Val>
-    bool exist(const Map& m, const Key& k, const Val& t) {
+    bool exist(const Map& m, const Key& k, const Val& t)
+    {
         auto er = m.equal_range(k);
         for (auto it = er.first; it != er.second; ++it) {
             if (it->second == t) {
@@ -76,7 +77,8 @@ namespace {
     }
 }  // namespace
 
-void InSliceDeghosting::blob_quality_ident(const cluster_graph_t& cg, vertex_tags_t& blob_tags) {
+void InSliceDeghosting::blob_quality_ident(const cluster_graph_t& cg, vertex_tags_t& blob_tags)
+{
     for (const auto& vtx : GraphTools::mir(boost::vertices(cg))) {
         const auto& node = cg[vtx];
         if (node.code() != 'b') {
@@ -94,7 +96,7 @@ void InSliceDeghosting::blob_quality_ident(const cluster_graph_t& cg, vertex_tag
         /// XIN: add between slice b-b connection info.
         /// follow b-b edges -> time, charge of connected blobs
         std::vector<double> fronts, backs;
-        for(auto v : neighbors_oftype<cluster_node_t::blob_t>(cg, vtx)) {
+        for (auto v : neighbors_oftype<cluster_node_t::blob_t>(cg, vtx)) {
             const auto ib = get<cluster_node_t::blob_t>(cg[v].ptr);
             if (ib->slice()->start() > current_t) {
                 backs.push_back(ib->value());
@@ -107,15 +109,16 @@ void InSliceDeghosting::blob_quality_ident(const cluster_graph_t& cg, vertex_tag
         log->debug("{} fronts {} backs {}", vtx, fronts.size(), backs.size());
     }
 }
-void InSliceDeghosting::local_deghosting(const cluster_graph_t& cg, vertex_tags_t& blob_tags) {
+void InSliceDeghosting::local_deghosting(const cluster_graph_t& cg, vertex_tags_t& blob_tags)
+{
     int count_keeper = 0;
     for (const auto& svtx : GraphTools::mir(boost::vertices(cg))) {
         if (cg[svtx].code() != 's') {
             continue;
         }
         // Loop over the neighbor blobs of current slice. Keep the largest one.
-        cluster_vertex_t max_blob = -1;
-        double max_charge = -1e12;
+        std::unordered_map<size_t, std::unordered_set<cluster_vertex_t> > view_groups;
+        std::unordered_map<RayGrid::grid_index_t, size_t> grid_occupancy;
         for (auto bedge : GraphTools::mir(boost::out_edges(svtx, cg))) {
             /// XIN: bvtx -> RayGrid::Strip::grid_range_t, {2views}, {3views}
             /// CHECK: per slice map {RayGrid::grid_index_t -> float}
@@ -124,19 +127,34 @@ void InSliceDeghosting::local_deghosting(const cluster_graph_t& cg, vertex_tags_
                 THROW(ValueError() << errmsg{
                           String::format("'s' node connnected to '%s' not implemented!", cg[bvtx].code())});
             }
-            // temp tag all TO_BE_REMOVED
-            blob_tags.insert({bvtx, TO_BE_REMOVED});
-            const auto iblob = get<cluster_node_t::blob_t>(cg[bvtx].ptr);
-            auto charge = iblob->value();
-            if (charge > max_charge) {
-                max_charge = charge;
-                max_blob = bvtx;
+
+            /// group blobs into #live view group
+            /// ASSUMPTION: connected meas node == # live views
+            auto meas = neighbors_oftype<cluster_node_t::meas_t>(cg, bvtx);
+            view_groups[meas.size()].insert(bvtx);
+
+            /// EXAMPLE: add up #blobs using each RayGrid::grid_index_t
+            const IBlob::pointer iblob = get<cluster_node_t::blob_t>(cg[bvtx].ptr);
+            const auto& strips = iblob->shape().strips();
+            for (const RayGrid::Strip& strip : strips) {
+                for (RayGrid::grid_index_t gridx = strip.bounds.first; gridx < strip.bounds.second; ++gridx) {
+                    if (grid_occupancy.find(gridx) != grid_occupancy.end()) {
+                        grid_occupancy[gridx]++;
+                    }
+                    else {
+                        grid_occupancy[gridx] = 1;
+                    }
+                }
             }
+
+            /// EXAMPLEONLY: temp tag all as TO_BE_REMOVED
+            blob_tags.insert({bvtx, TO_BE_REMOVED});
         }
-        /// rm the keeper from TO_BE_REMOVED
+        /// EXAMPLEONLY: rm 3views from TO_BE_REMOVED
+        /// this is to prune all 3views from blob_tags, for single key deletion, use equal_range.
         for (auto i = blob_tags.begin(), last = blob_tags.end(); i != last;) {
             auto const& [key, value] = *i;
-            if (key == max_blob && value == TO_BE_REMOVED) {
+            if (view_groups[3].find(key) != view_groups[3].end() && value == TO_BE_REMOVED) {
                 i = blob_tags.erase(i);
                 ++count_keeper;
             }
@@ -173,9 +191,7 @@ bool InSliceDeghosting::operator()(const input_pointer& in, output_pointer& out)
     /// FIXME: need checks.
     using VFiltered =
         typename boost::filtered_graph<cluster_graph_t, boost::keep_all, std::function<bool(cluster_vertex_t)> >;
-    VFiltered fg_rm_bad_blobs(in_graph, {}, [&](auto vtx) {
-        return !exist(blob_tags, vtx, TO_BE_REMOVED);
-    });
+    VFiltered fg_rm_bad_blobs(in_graph, {}, [&](auto vtx) { return !exist(blob_tags, vtx, TO_BE_REMOVED); });
 
     /// 4, in-group clustering. in: ICluster + blob_quality_tags out: filtered ICluster
     /// FIXME: place holder.
@@ -186,15 +202,17 @@ bool InSliceDeghosting::operator()(const input_pointer& in, output_pointer& out)
     dump_cg(cg_old_bb, log);
     /// rm old b-b edges
     using EFiltered =
-        typename boost::filtered_graph<cluster_graph_t, std::function<bool(cluster_edge_t)>, boost::keep_all >;
-    EFiltered fg_no_bb(cg_old_bb, [&](auto edge) {
-        auto source = boost::source(edge, cg_old_bb);
-        auto target = boost::target(edge, cg_old_bb);
-        if (cg_old_bb[source].code()=='b' and cg_old_bb[target].code()=='b') {
-            return false;
-        }
-        return true;
-    }, {});
+        typename boost::filtered_graph<cluster_graph_t, std::function<bool(cluster_edge_t)>, boost::keep_all>;
+    EFiltered fg_no_bb(cg_old_bb,
+                       [&](auto edge) {
+                           auto source = boost::source(edge, cg_old_bb);
+                           auto target = boost::target(edge, cg_old_bb);
+                           if (cg_old_bb[source].code() == 'b' and cg_old_bb[target].code() == 'b') {
+                               return false;
+                           }
+                           return true;
+                       },
+                       {});
     WireCell::cluster_graph_t cg_new_bb;
     boost::copy_graph(fg_no_bb, cg_new_bb);
     log->debug("cg_new_bb:");
@@ -202,8 +220,7 @@ bool InSliceDeghosting::operator()(const input_pointer& in, output_pointer& out)
     /// make new b-b edges within groups
     std::vector<gc_filter_t> filters = {
         [&](const cluster_vertex_t& vtx) { return exist(blob_tags, vtx, GOOD) && !exist(blob_tags, vtx, BAD); },
-        [&](const cluster_vertex_t& vtx) { return !exist(blob_tags, vtx, GOOD) && exist(blob_tags, vtx, BAD); }
-    };
+        [&](const cluster_vertex_t& vtx) { return !exist(blob_tags, vtx, GOOD) && exist(blob_tags, vtx, BAD); }};
     /// each filter adds new b-b edges on cg_new_bb
     for (auto filter : filters) {
         geom_clustering(cg_new_bb, m_clustering_policy, filter);
