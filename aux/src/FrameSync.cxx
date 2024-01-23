dbeda5e56965f10d0a66877894e0555e7ea4208a
HaiwangYu <yuhw.pku@gmail.com>
2024-01-23 01:04:30 -0500

    works, but could miss stuff

--- aux/src/FrameSync.cxx
+++ aux/src/FrameSync.cxx
@@ -9,7 +9,9 @@ WIRECELL_FACTORY(FrameSync,
 
 using namespace WireCell;
 
-Aux::FrameSync::FrameSync(const size_t multiplicity) : m_multiplicity(multiplicity) { }
+Aux::FrameSync::FrameSync(const size_t multiplicity)
+    : Aux::Logger("FrameSync", "glue")
+    , m_multiplicity(multiplicity) { }
 Aux::FrameSync::~FrameSync() { }
 
 std::vector<std::string> Aux::FrameSync::input_types()
@@ -21,15 +23,17 @@ std::vector<std::string> Aux::FrameSync::input_types()
 
 bool Aux::FrameSync::operator()(input_queues& iqs, output_queues& oqs)
 {
+    log->debug("FrameSync: iqs size: {} oqs size: {}", iqs.size(), std::tuple_size<output_queues>::value);
+
     size_t neos=0, nmin=0;
     int min_ident=0;
     size_t min_index=0;
 
     const size_t nin = iqs.size();
     for (size_t ind=0; ind<nin; ++ind) {
-        auto& iq = iqs[0];
+        auto& iq = iqs[ind];
         if (iq.empty()) {
-            return true;
+            continue;
         }
         auto frame = iq.front();
         if (!frame) {
@@ -43,17 +47,8 @@ bool Aux::FrameSync::operator()(input_queues& iqs, output_queues& oqs)
         }
     }
 
-    if (neos == iqs.size()) {   // we have EOS sync
-        for (auto& iq : iqs) {
-            iq.pop_front();     // pop all EOS
-        }
-        std::get<0>(oqs).push_back(nullptr); // forward EOS
-
-        // May have more behind the EOS
-        return (*this)(iqs, oqs);
-    }
-        
-    if (neos+nmin == nin) {     // fully populated
+    // found min (good)
+    if (nmin > 0) {
         auto frame = iqs[min_index].front();
         iqs[min_index].pop_front();
         std::get<0>(oqs).push_back(frame);
@@ -62,8 +57,20 @@ bool Aux::FrameSync::operator()(input_queues& iqs, output_queues& oqs)
         return (*this)(iqs, oqs);
     }
 
-    // If we reach here then we do not have fully populated input
-    // streams and must wait for more.
-    
+    // push eos
+    for (size_t ind=0; ind<nin; ++ind) {
+        auto& iq = iqs[ind];
+        if (iq.empty()) {
+            continue;
+        }
+        auto frame = iq.front();
+        if (!frame) {
+            std::get<0>(oqs).push_back(nullptr); // forward EOS
+            iq.pop_front();
+            return (*this)(iqs, oqs);
+        }
+    }
+
+    /// FIXEME: check all empty
     return true;
 }
